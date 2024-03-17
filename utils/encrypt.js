61b8774eb21d91ca3c59534836e05b409f0b3d43
MKMalik <engimkmalik@gmail.com>
2024-03-18 00:30:11 +0530

    bcrypt-node
    - bcrypt-node package used, removed bcrypt, bcrypt was not going with binary compiled

--- utils/encrypt.js
+++ utils/encrypt.js
@@ -1,15 +1,22 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt-nodejs");
+const { promisify } = require("util");
+
+// Promisify bcrypt functions
+const hashAsync = promisify(bcrypt.hash);
+const compareAsync = promisify(bcrypt.compare);
 
 async function calculateHash(value) {
   const saltRounds = 10; // Adjust this according to your security requirements
-  const hashedValue = await bcrypt.hash(value, saltRounds);
+  const hashedValue = await hashAsync(value, null, null);
   return hashedValue;
 }
+
 async function verifyHash(hashedValue, plainText) {
-  return await bcrypt.compare(plainText, hashedValue);
+  return await compareAsync(plainText, hashedValue);
 }
 
 module.exports = {
   calculateHash,
   verifyHash,
-}
+};
+
